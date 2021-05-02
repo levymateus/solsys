1427e0de61eacace866253e1f2604565dd8e8201
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2021-05-02 10:14:22 -0300

    remove unused files

--- src/ecs/utils.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const camelize = (str) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => (index === 0 ? word.toLowerCase() : word.toUpperCase())).replace(/\s+/g, '');
